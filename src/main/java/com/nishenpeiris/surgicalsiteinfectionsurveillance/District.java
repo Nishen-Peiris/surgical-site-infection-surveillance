ef2ac6cf25c3434b6f97d477e732f80f3442a37b
Nishen Peiris <Nishen Peiris>
2017-07-19 10:43:21 +0530

    Add Patient.java, DetailsOfReferral.java and other required enums

---
+++ src/main/java/com/nishenpeiris/surgicalsiteinfectionsurveillance/District.java
@@ -0,0 +1,10 @@
+package com.nishenpeiris.surgicalsiteinfectionsurveillance;
+
+/**
+ * Enum to represent the district of a patient
+ *
+ * @author Nishen Peiris
+ */
+public enum District {
+    AMPARA, ANURADHAPURA, BADULLA, BATTICALOA, COLOMBO, GALLE, GAMPAHA, HAMBANTOTA, JAFFNA, KALUTARA, KANDY, KEGALLE, KILINOCHCHI, KURUNEGALA, MANNAR, MATALE, MATARA, MONARAGALA, MULLATIVU, NUWARA_ELIYA, POLONNARUWA, PUTTALAM, RATNAPURA, TRINCOMALEE, VAVUNIYA
+}
